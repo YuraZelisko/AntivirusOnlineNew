67cc7c91e48113d85d1e2871708381883177981c
YuraZelisko <meteoritKIDS@gmail.com>
2017-08-04 11:30:16 +0300

    Update MailSenderServiceImpl.java

--- src/main/java/com/antivirus/serviceImpl/MailSenderServiceImpl.java
+++ src/main/java/com/antivirus/serviceImpl/MailSenderServiceImpl.java
@@ -14,7 +14,7 @@ import java.util.Properties;
 @Service
 public class MailSenderServiceImpl implements MailSenderService {
     private final String EMAIL_FROM = "meteoritKIDS@gmail.com";
-    private final String EMAIL_PASSWORD = "limonadik";
+    private final String EMAIL_PASSWORD = "*********";
 
     @Override
     public void sendMail(String theme, String mailBody, String email) {
